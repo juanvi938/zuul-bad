d0452ae1172a1b079cad6eb89c73a842488260f9
Juan Vi <juan.vi938@gmail.com>
2014-05-02 12:28:02 +0200

    0119 Final: Modificada clase Game para que utilice la clase Stack y poder recorrer todas las localizaciones anteriores

--- Game.java
+++ Game.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.Stack;
 /**
  *  This class is the main class of the "World of Zuul" application. 
  *  "World of Zuul" is a very simple, text based adventure game.  Users 
@@ -20,7 +20,7 @@ public class Game
 {
     private Parser parser;
     private Room currentRoom;
-    private ArrayList<Room> visitedRooms;
+    private Stack<Room> visitedRooms;
         
     /**
      * Create the game and initialise its internal map.
@@ -29,7 +29,7 @@ public class Game
     {
         createRooms();
         parser = new Parser();
-        visitedRooms = new ArrayList<>();
+        visitedRooms = new Stack<>();
     }
 
     /**
@@ -95,7 +95,6 @@ public class Game
         boolean finished = false;
         while (! finished) {
             Command command = parser.getCommand();
-            visitedRooms.add(currentRoom);
             finished = processCommand(command);
         }
         System.out.println("Thank you for playing.  Good bye.");
@@ -136,6 +135,7 @@ public class Game
             printHelp();
         }
         else if (commandWord.equals("go")) {
+            visitedRooms.push(currentRoom);
             goRoom(command);
         }
         else if (commandWord.equals("quit")) {
@@ -145,7 +145,7 @@ public class Game
         }else if(commandWord.equals("eat")) {
             System.out.println("You have eaten now and you are not hungry any more");
         }else if(commandWord.equals("back")) {
-            currentRoom = visitedRooms.get(visitedRooms.size() - 2);
+            currentRoom = visitedRooms.pop();
             printLocationInfo();
         }
 
