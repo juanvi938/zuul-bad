f1c01994400832e46ac6df2df782f5f2cfbb862f
Juan Vi <juan.vi938@gmail.com>
2014-05-16 12:48:42 +0200

    Añadida clase Option de tipo enum; modificada clase CommandWords para que use un HashMap para guardar los comandos en String y sus correspondientes Options

--- CommandWords.java
+++ CommandWords.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 /**
  * This class is part of the "World of Zuul" application. 
  * "World of Zuul" is a very simple, text based adventure game.  
@@ -12,16 +13,23 @@
 public class CommandWords
 {
     // a constant array that holds all valid command words
-    private static final String[] validCommands = {
-        "go", "quit", "help", "look", "eat", "back", "take", "drop", "items"
-    };
+    private static HashMap<String,Option> validCommands;
 
     /**
      * Constructor - initialise the command words.
      */
     public CommandWords()
     {
-        // nothing to do at the moment...
+        validCommands = new HashMap<>();
+        validCommands.put("go",Option.GO);
+        validCommands.put("quit",Option.QUIT);
+        validCommands.put("help",Option.HELP);
+        validCommands.put("look",Option.LOOK);
+        validCommands.put("eat",Option.EAT);
+        validCommands.put("back",Option.BACK);
+        validCommands.put("take",Option.TAKE);
+        validCommands.put("drop",Option.DROP);
+        validCommands.put("items",Option.ITEMS);
     }
 
     /**
@@ -31,12 +39,8 @@ public class CommandWords
      */
     public boolean isCommand(String aString)
     {
-        for(int i = 0; i < validCommands.length; i++) {
-            if(validCommands[i].equals(aString))
-                return true;
-        }
-        // if we get here, the string was not found in the commands
-        return false;
+        boolean booleanToReturn = validCommands.containsKey(aString);
+        return booleanToReturn;
     }
     
     /**
@@ -44,9 +48,6 @@ public class CommandWords
      */
     public void showAll()
     {
-        for(int i = 0; i < validCommands.length; i++)
-        {
-            System.out.println(validCommands[i]);
-        }
+        System.out.println(validCommands.keySet());
     }
 }
